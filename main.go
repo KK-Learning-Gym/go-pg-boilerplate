3faa7ea8b4933219b255f5a63251ae9c54ff6e85
Kunal Kundu <kunal99kundu@gmail.com>
2023-07-08 20:00:00 +0530

    add queries

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"reflect"
 
 	"github.com/tinfoil-knight/go-pg-boilerplate/sqlc"
 
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func main() {
+func run() error {
+	ctx := context.Background()
+
 	db, err := pgxpool.New(context.Background(), "postgresql://localhost:5432/mydb")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
@@ -19,4 +24,37 @@ func main() {
 	defer db.Close()
 
 	queries := sqlc.New(db)
+
+	// list all authors
+	authors, err := queries.ListAuthors(ctx)
+	if err != nil {
+		return err
+	}
+	log.Println(authors)
+
+	// create an author
+	insertedAuthor, err := queries.CreateAuthor(ctx, sqlc.CreateAuthorParams{
+		Name: "Brian Kernighan",
+		Bio:  pgtype.Text{String: "Co-author of The C Programming Language and The Go Programming Language", Valid: true},
+	})
+	if err != nil {
+		return err
+	}
+	log.Println(insertedAuthor)
+
+	// get the author we just inserted
+	fetchedAuthor, err := queries.GetAuthor(ctx, insertedAuthor.ID)
+	if err != nil {
+		return err
+	}
+
+	// prints true
+	log.Println(reflect.DeepEqual(insertedAuthor, fetchedAuthor))
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
